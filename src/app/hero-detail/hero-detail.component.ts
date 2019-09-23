04daac2c04dea0164aa4074d60d1884cbe64a53a
Somló Panna <somlo.panna@ENCOSOFT.INTERNAL>
2019-09-23 14:41:12 +0200

    Refactoring the original HeroesComponent into two components  commit

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+/* hero property must be an Input property
+* external HeroesComponent will bind to it
+*/
+import { Hero } from '../hero' ; /*The HeroDetailComponent template binds to the component's hero property which is of type Hero*/
 
 @Component({
   selector: 'app-hero-detail',
@@ -6,6 +10,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-detail.component.scss']
 })
 export class HeroDetailComponent implements OnInit {
+
+  @Input() hero: Hero ; /* input property, mivel a szülő komponensből kapja meg az ott kiválasztott hero-t */
+  /* This component simply receives a hero object through its hero property and displays it. */
 
   constructor() { }
 
